261bd8d5efaa8529624aefa324f699853d8a98e4
William Pereira <MetalHexx@outlook.com>
2022-09-07 01:34:14 -0400

    added game board code

---
+++ Nibbles/GameBoard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nibbles
+{
+    public  class GameBoard
+    {
+        public int MinX { get; private set; } = -1;
+        public int MaxX { get; private set; } = Console.BufferWidth;
+        public int MinY { get; private set; } = -1;
+        public int MaxY { get; private set; } = Console.BufferHeight;
+    }
+}
