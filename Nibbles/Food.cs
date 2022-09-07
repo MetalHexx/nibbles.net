c53645f7cda2d03f3508fc0858f4c19f22bbcd8e
William Pereira <MetalHexx@outlook.com>
2022-09-07 01:34:02 -0400

    added food code

---
+++ Nibbles/Food.cs
@@ -0,0 +1,17 @@
+﻿namespace Nibbles
+{
+    public record Food
+    {
+        public GameObjectPosition Position { get; init; }
+
+        public Food(int x, int y)
+        {
+            Position = new GameObjectPosition(x, y);
+        }
+
+        public Food(GameObjectPosition position)
+        {
+            Position = position;
+        }
+    }
+}
