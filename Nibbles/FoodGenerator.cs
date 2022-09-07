6c11ff8a899c17a1caa4933cbcd16aaf6b531262
William Pereira <MetalHexx@outlook.com>
2022-09-07 01:34:46 -0400

    added food generator and screen write

---
+++ Nibbles/FoodGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nibbles
+{
+    public class FoodGenerator : ScreenWriter
+    {
+        private Random _random = new Random();
+        public Food? Next(IEnumerable<GameObjectPosition> excludedPositions)
+        {
+            var newFoodPosition = GetRandomPosition();
+
+            while (excludedPositions.Any(position => position == newFoodPosition))
+            {
+                newFoodPosition = GetRandomPosition();
+            }
+            WriteCharacter('F', ConsoleColor.Red, newFoodPosition.XPosition, newFoodPosition.YPosition);
+            return new Food(newFoodPosition);
+        }
+
+        public GameObjectPosition GetRandomPosition() => 
+            new GameObjectPosition(_random.Next(1, 10), _random.Next(1, 10));
+    }
+}
---
+++ Nibbles/ScreenWriter.cs
@@ -0,0 +1,17 @@
+﻿namespace Nibbles
+{
+    public class ScreenWriter
+    {
+        public ScreenWriter()
+        {
+            Console.CursorVisible = false;
+        }
+        protected void WriteCharacter(char character, ConsoleColor color, int xPosition, int yPosition)
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(xPosition, yPosition);
+            Console.Write(character);
+            Console.ResetColor();
+        }
+    }
+}
