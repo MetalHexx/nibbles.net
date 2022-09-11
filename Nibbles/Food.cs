2edd89382a1e991d6de14cf72abe7d6722ceea24
William Pereira <MetalHexx@outlook.com>
2022-09-10 20:23:55 -0400

    Took position generation logic out of food

--- Nibbles/Food.cs
+++ Nibbles/Food.cs
@@ -6,28 +6,14 @@
 
         private Random _random = new Random();
 
-        public Food(int x, int y, GameObjectPosition[] excludedPositions)
+        public Food(GameObjectPosition position)
         {
-            Position = GetRandomPosition(x, y, excludedPositions);
+            Position = position;
         }
 
         public Food(GameObjectPosition position, int number)
         {
             Position = position;
         }
-
-        private GameObjectPosition GetRandomPosition(int maxXPosition, int maxYPosition, params GameObjectPosition[] excludedPositions)
-        {
-            var newFoodPosition = GetRandomPosition(maxXPosition, maxYPosition);
-
-            while (excludedPositions.Any(position => position == newFoodPosition))
-            {
-                newFoodPosition = GetRandomPosition(maxXPosition, maxYPosition);
-            }
-            return newFoodPosition;
-        }
-
-        private GameObjectPosition GetRandomPosition(int maxXPosition, int maxYPosition) =>
-            new GameObjectPosition(_random.Next(1, maxXPosition), _random.Next(1, maxYPosition));
     }
 }
