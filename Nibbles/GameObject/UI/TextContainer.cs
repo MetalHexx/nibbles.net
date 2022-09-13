4d2d52542a3ed81f4c6082683cbb9cc54d1d397b
William Pereira <MetalHexx@outlook.com>
2022-09-13 00:43:31 -0400

    Rename Text classes

---
+++ Nibbles/GameObject/UI/TextContainer.cs
@@ -0,0 +1,48 @@
+﻿using Nibbles.GameObject.Abstractions;
+using Nibbles.GameObject.Dimensions;
+
+namespace Nibbles.GameObject.UI
+{
+    public class GameText : SpriteContainer
+    {
+        private string _text;
+
+        public GameText(Position position, string text, ConsoleColor foregroundColor, ConsoleColor backgroundColor)
+            : base(position, foregroundColor, backgroundColor)
+        {
+            _text = text;
+            Build();
+        }
+
+        public void SetText(string text)
+        {
+            var lengthDelta = _text.Length - text.Length;
+            _text = text;
+            PadWhiteSpace(lengthDelta);
+            Build();
+        }
+
+        private void PadWhiteSpace(int padAmount)
+        {
+            if (padAmount > 0)
+            {
+                _text = _text.PadRight(padAmount);
+            }
+        }
+
+        private void Build()
+        {
+            _sprites.Clear();
+            var charList = _text.ToList();
+
+            for (int i = 0; i < charList.Count; i++)
+            {
+                var position = Position with
+                {
+                    XPosition = Position.XPosition + i
+                };
+                _sprites.Add(new TextSprite(position, ForegroundColor, BackgroundColor, charList[i]));
+            }
+        }
+    }
+}
--- Nibbles/GameObject/UI/TextSprite.cs
+++ Nibbles/GameObject/UI/TextSprite.cs
@@ -3,46 +3,9 @@ using Nibbles.GameObject.Dimensions;
 
 namespace Nibbles.GameObject.UI
 {
-    public class GameText : SpriteContainer
+    public record TextSprite : Sprite
     {
-        private string _text;
-
-        public GameText(Position position, string text, ConsoleColor foregroundColor, ConsoleColor backgroundColor)
-            : base(position, foregroundColor, backgroundColor)
-        {
-            _text = text;
-            Build();
-        }
-
-        public void SetText(string text)
-        {
-            var lengthDelta = _text.Length - text.Length;
-            _text = text;
-            PadWhiteSpace(lengthDelta);
-            Build();
-        }
-
-        private void PadWhiteSpace(int padAmount)
-        {
-            if (padAmount > 0)
-            {
-                _text = _text.PadRight(padAmount);
-            }
-        }
-
-        private void Build()
-        {
-            _sprites.Clear();
-            var charList = _text.ToList();
-
-            for (int i = 0; i < charList.Count; i++)
-            {
-                var position = Position with
-                {
-                    XPosition = Position.XPosition + i
-                };
-                _sprites.Add(new TextSpritePart(position, ForegroundColor, BackgroundColor, charList[i]));
-            }
-        }
+        public TextSprite(Position position, ConsoleColor foregroundColor, ConsoleColor backgroundColor, char displayCharacter)
+            : base(position, foregroundColor, backgroundColor, displayCharacter) { }
     }
 }
